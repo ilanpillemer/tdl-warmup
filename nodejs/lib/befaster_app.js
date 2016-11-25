fe288607f9cc3da34e188adfb32bf256c0ec68d0
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2016-11-25 18:05:46 +0000

    Add description to Node.js

--- nodejs/lib/befaster_app.js
+++ nodejs/lib/befaster_app.js
@@ -2,10 +2,43 @@
 var RunnerActions = require('./runner/runner_actions');
 var startClient = require('./runner/client_runner');
 
-
-// If you are ready to go live:  npm start ready
-
-
+/**
+ * ~~~~~~~~~~ Running the system: ~~~~~~~~~~~~~
+ *
+ *   From command line:
+ *      npm start -Daction=$ACTION
+ *
+ *   From IDE:
+ *      Set the value of the `actionIfNoArgs`
+ *      Run this class from IDE.
+ *
+ *   Available actions:
+ *        * getNewRoundDescription - Get the round description (call once per round).
+ *        * testConnectivity       - Test you can connect to the server (call any number of time)
+ *        * deployToProduction     - Release your code. Real requests will be used to test your solution.
+ *                                   If your solution is wrong you get a penalty of 10 minutes.
+ *                                   After you fix the problem, you should deploy a new version into production.
+ *
+ * ~~~~~~~~~~ The workflow ~~~~~~~~~~~~~
+ *
+ *   +------+----------------------------------------+-----------------------------------------------+
+ *   | Step |      IDE                               |         Web console                           |
+ *   +------+----------------------------------------+-----------------------------------------------+
+ *   |  1.  |                                        | Start a session: http://run.befaster.io:8111  |
+ *   |  2.  |                                        | Configure your email                          |
+ *   |  3.  | Set the email variable                 |                                               |
+ *   |  4.  |                                        | Start a challenge, should display "Started"   |
+ *   |  5.  | Run "getNewRoundDescription"           |                                               |
+ *   |  6.  | Read description from ./challenges     |                                               |
+ *   |  7.  | Implement the required method in       |                                               |
+ *   |      |   ./lib/solutions                      |                                               |
+ *   |  8.  | Run "testConnectivity", observe output |                                               |
+ *   |  9.  | If ready, run "deployToProduction"     |                                               |
+ *   | 10.  |                                        | Type "done"                                   |
+ *   | 11.  |                                        | Check failed requests                         |
+ *   | 12.  |                                        | Go to step 5.                                 |
+ *   +------+----------------------------------------+-----------------------------------------------+
+ */
 var args = process.argv.slice(2, process.argv.length);
 startClient(args, {
     email: 'your_email_here',
